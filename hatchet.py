694a79e5670f2b26a1dfdfa2874460772d534943
stower <stower@plos.org>
2017-06-26 14:15:38 -0700

    implement door object lists. Total doors found

--- hatchet.py
+++ hatchet.py
@@ -26,6 +26,18 @@ tile_array2 = [
 ]
 
 
+class Door:
+    origin = (0, 0)
+    width = 0
+
+    def __init__(self, origin, width):
+        self.origin = origin
+        self.width = width
+
+    def __str__(self):
+        return str(self.origin) + " , " + str(self.width)
+
+
 def stamp_tile(tile_array, offset, floor_array):
     x = offset[0]
     y = offset[1]
@@ -36,7 +48,7 @@ def stamp_tile(tile_array, offset, floor_array):
 
             # tile is a door. Add it do the available door list
             if tile_array[i][j] == 2:
-                door_positions.append({j + x, i + y})
+                door_positions.append((j + x, i + y))
 
     return floor_array
 
@@ -48,7 +60,11 @@ def find_door_width():
     # determine length of doors by getting count of matching y values
     for i in range(0, len(door_positions)):
         # continuous door is found
-        next_door = door_positions[i + 1]
+        if i + 1 == len(door_positions):
+            next_door = None
+        else:
+            next_door = door_positions[i + 1]
+
         this_door = door_positions[i]
         if next_door is not None and this_door[0] == next_door[0] - 1 and this_door[1] == next_door[1]:
             size = size + 1
@@ -58,12 +74,13 @@ def find_door_width():
                 # first door, increase size by one
 
                 door_origin_position = door_positions[i - size]
-                door_origin_position.width = size + 1
+                # door_origin_position.width = size + 1
 
-                horizontal_doors.append(door_origin_position)
+                door = Door(door_origin_position, size + 1)
+                horizontal_doors.append(door)
                 size = 0
             else:
-                horizontal_one_width_doors.append(this_door)
+                horizontal_one_width_doors.append(Door(this_door, 1))
 
     # door_positions.sort(mysortfunction)
 
@@ -73,7 +90,11 @@ def find_door_width():
     # determine length of doors by getting count of matching x values
     for j in range(0, len(door_positions)):
         # continuous door is found
-        next_door = door_positions[j + 1]
+        if j + 1 == len(door_positions):
+            next_door = None
+        else:
+            next_door = door_positions[j + 1]
+
         this_door = door_positions[j]
         if next_door is not None and this_door[1] == next_door[1] - 1 and this_door[0] == next_door[0]:
             size = size + 1
@@ -83,23 +104,20 @@ def find_door_width():
                 # first door, increase size by one
 
                 door_origin_position = door_positions[j - size]
-                door_origin_position.width = size + 1
-
-                vertical_doors.append(door_origin_position)
+                door = Door(door_origin_position, size + 1)
+                vertical_doors.append(door)
                 size = 0
             else:
-                vertical_one_width_doors.append(this_door)
+                vertical_one_width_doors.append(Door(this_door, 1))
 
     one_width_doors = []
     for k in range(len(horizontal_one_width_doors)):
         for l in range(len(vertical_one_width_doors)):
-            if horizontal_one_width_doors[k][0] == vertical_one_width_doors[l][0] \
-                    and horizontal_one_width_doors[k][1] == vertical_one_width_doors[l][1]:
-                horizontal_one_width_doors[k].width = 1
+            if horizontal_one_width_doors[k].origin[0] == vertical_one_width_doors[l].origin[0] \
+                    and horizontal_one_width_doors[k].origin[1] == vertical_one_width_doors[l].origin[1]:
                 one_width_doors.append(horizontal_one_width_doors[k])
 
-    wide_doors = horizontal_doors + vertical_doors
-    return wide_doors + one_width_doors
+    return horizontal_doors + vertical_doors + one_width_doors
 
 
 def main():
@@ -112,11 +130,17 @@ def main():
 
     pretty_print(rotate(tile_array1))
 
-    floor_array = stamp_tile(tile_array1, (7, 7), floor_array)
-    floor_array = stamp_tile(tile_array1, (7, 11), floor_array)
+    floor_array = stamp_tile(tile_array1, (0, 7), floor_array)
+    floor_array = stamp_tile(rotate(tile_array1), (7, 11), floor_array)
     pretty_print(floor_array)
 
     print(door_positions)
+
+    doors = find_door_width()
+    for door in doors:
+        print(str(door))
+
+    print('DONE')
 
 
 if __name__ == '__main__':
